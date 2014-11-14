b2a163147a2329f656085941e2b3e81ff298280a
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-11-14 16:54:43 +0000

    Started implementing Model's LoadFromFile
    |SOURCE|
    [Common/DataTypes]
    * Added a FILE_CHUNK struct for files to use, each chunk has an ID and the
      number of bytes which follow it
    * FILE_END_CHUNK should be present at the end of each chunk, it's a crude kind
      of error checking
    [Common/Model]
    * LoadFromFile will load a hypothetical model's header, then crash as there is
      no code to read data from chunks
    * Added a MODEL_HEADER struct containing vital information for creating a model
    * Added a MESH_DATA struct containing the ID of the mesh and the MD5 digest of
      the material to apply

--- Source/Common/Headers/Model.hpp
+++ Source/Common/Headers/Model.hpp
@@ -2,12 +2,30 @@
 #define __FPS_MODEL_HPP__
 
 #include <DataTypes.hpp>
+#include <MD5.hpp>
 #include <string>
 
 namespace FPS
 {
 	class Renderer;
 	class Matrix4x4;
+
+	const FPS_UINT32 MESH_CHUNK		= 0x00000001;
+	const FPS_UINT32 JOINT_CHUNK	= 0x00000002;
+
+	typedef struct _tagMODEL_HEADER
+	{
+		char		ID[ 4 ];
+		FPS_UINT32	MeshCount;
+		FPS_UINT32	MaterialCount;
+		FPS_UINT32	JointCount;
+	}MODEL_HEADER,*PMODEL_HEADER;
+
+	typedef struct _tagMESHL_DATA
+	{
+		FPS_UINT32	ID;
+		MD5_DIGEST	MaterialMD5;
+	}MESH_DATA,*PMESH_DATA;
 
 	class Model
 	{
@@ -20,7 +38,11 @@ namespace FPS
 		FPS_UINT32 Render( );
 
 	private:
+		FPS_UINT32 ReadChunk( FILE_CHUNK &p_Chunk );
+
 		Renderer	*m_pRenderer;
+		MESH_DATA	*m_pMeshData;
+		FPS_UINT32	m_MeshCount;
 	};
 }
 
--- Source/Common/Source/Model.cpp
+++ Source/Common/Source/Model.cpp
@@ -1,22 +1,90 @@
 #include <Model.hpp>
+#include <Renderer.hpp>
+#include <Memory.hpp>
+#include <iostream>
+#include <cstdio>
 
 namespace FPS
 {
-	Model::Model( Renderer *p_pRenderer )
+	Model::Model( Renderer *p_pRenderer ) :
+		m_pRenderer( p_pRenderer ),
+		m_pMeshData( FPS_NULL ),
+		m_MeshCount( 0U )
 	{
 	}
 
 	Model::~Model( )
 	{
+		SafeDeleteArray< MESH_DATA >( m_pMeshData );
 	}
 
 	FPS_UINT32 Model::LoadFromFile( const std::string &p_FileName )
 	{
+		FILE *pModelFile = fopen( p_FileName.c_str( ), "rb" );
+
+		if( pModelFile == FPS_NULL )
+		{
+			std::cout << "[FPS::Model::LoadFromFile] <ERROR> Failed to open "
+				"file " << p_FileName << " for reading" << std::endl;
+
+			return FPS_FAIL;
+		}
+
+		fseek( pModelFile, 0, SEEK_END );
+		FPS_UINT32 BytesToGo = ftell( pModelFile );
+		rewind( pModelFile );
+
+		// TODO
+		// Get the header
+		// Then get the meta data
+		// Then the meshes
+		// Finally, the joints
+		MODEL_HEADER ModelHeader;
+		fread( &ModelHeader, sizeof( ModelHeader ), 1, pModelFile );
+
+		BytesToGo -= sizeof( ModelHeader );
+
+		m_MeshCount = ModelHeader.MeshCount;
+		m_pMeshData = new MESH_DATA[ m_MeshCount ];
+
+		FILE_CHUNK Chunk;
+		fread( &Chunk, sizeof( FILE_CHUNK ), 1, pModelFile );
+
+		FPS_UINT32 ChunkSkip = sizeof( FILE_CHUNK );
+
+		// Get chunks
+		while( BytesToGo )
+		{
+			BytesToGo -= ChunkSkip;
+			if( ReadChunk( Chunk ) != FPS_OK )
+			{
+				return FPS_FAIL;
+			}
+			BytesToGo -= Chunk.Size;
+		}
+
+		if( pModelFile )
+		{
+			fclose( pModelFile );
+			pModelFile = FPS_NULL;
+		}
+
 		return FPS_FAIL;
 	}
 
 	FPS_UINT32 Model::Render( )
 	{
+		std::cout << "[FPS::Model::Render] <ERROR> NOT IMPLEMENTED" <<
+			std::endl;
+
+		return FPS_FAIL;
+	}
+
+	FPS_UINT32 Model::ReadChunk( FILE_CHUNK &p_Chunk )
+	{
+		std::cout << "[FPS::Model::ReadChunk] <ERROR> NOT IMPLEMENTED" <<
+			std::endl;
+
 		return FPS_FAIL;
 	}
 }
