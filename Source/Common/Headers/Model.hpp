ab29a3e4515de4107e93d69b1b045be3eda832a1
Rico Tyrell <teh_programerer@interactivelaboratories.com>
2014-11-15 01:12:08 +0000

    Model's ReadMeshData should be able to read the mesh data from a model file
    |SOURCE|
    [Common/Model]
    * Added a MODEL_VERTEX struct describing the format for vertices in a model
    * Added a MESH_INFORMATION struct, which contains the data from the file
      describing a mesh
    * Added ReadMeshData for when a MESH_CHUNK ID is found in the file

--- Source/Common/Headers/Model.hpp
+++ Source/Common/Headers/Model.hpp
@@ -13,6 +13,16 @@ namespace FPS
 	const FPS_UINT32 MESH_CHUNK		= 0x00000001;
 	const FPS_UINT32 JOINT_CHUNK	= 0x00000002;
 
+#pragma pack( 1 )
+	typedef struct _tagMODEL_VERTEX
+	{
+		FPS_FLOAT32	Position[ 3 ];
+		FPS_FLOAT32	Normal[ 3 ];
+		FPS_FLOAT32	ST[ 2 ];
+		FPS_UINT32	JointIndices[ 4 ];
+		FPS_FLOAT32	JointWeights[ 4 ];
+	}MODEL_VERTEX,*PMODEL_VERTEX;
+
 	typedef struct _tagMODEL_HEADER
 	{
 		char		ID[ 4 ];
@@ -21,11 +31,19 @@ namespace FPS
 		FPS_UINT32	JointCount;
 	}MODEL_HEADER,*PMODEL_HEADER;
 
+	typedef struct _tagMESH_INFORMATION
+	{
+		FPS_UINT32	VertexCount;
+		FPS_UINT32	IndexCount;
+		MD5_DIGEST	MaterialMD5;
+	}MESH_INFORMATION,*PMESH_INFORMATION;
+
 	typedef struct _tagMESHL_DATA
 	{
 		FPS_UINT32	ID;
 		MD5_DIGEST	MaterialMD5;
 	}MESH_DATA,*PMESH_DATA;
+#pragma pack( )
 
 	class Model
 	{
@@ -38,7 +56,9 @@ namespace FPS
 		FPS_UINT32 Render( );
 
 	private:
-		FPS_UINT32 ReadChunk( FILE_CHUNK &p_Chunk );
+		FPS_UINT32 ReadChunk( FILE_CHUNK &p_Chunk, FILE *p_pFile );
+		FPS_UINT32 ReadMeshData( FILE *p_pFile, const FPS_UINT32 p_MeshIndex,
+			FPS_UINT32 &p_BytesRead );
 
 		Renderer	*m_pRenderer;
 		MESH_DATA	*m_pMeshData;
--- Source/Common/Source/Model.cpp
+++ Source/Common/Source/Model.cpp
@@ -51,16 +51,51 @@ namespace FPS
 		fread( &Chunk, sizeof( FILE_CHUNK ), 1, pModelFile );
 
 		FPS_UINT32 ChunkSkip = sizeof( FILE_CHUNK );
+		FPS_UINT32 BytesRead = 0U;
+		FPS_UINT32 MeshIndex = 0;
 
 		// Get chunks
 		while( BytesToGo )
 		{
 			BytesToGo -= ChunkSkip;
-			if( ReadChunk( Chunk ) != FPS_OK )
+			if( ReadChunk( Chunk, pModelFile ) != FPS_OK )
 			{
 				return FPS_FAIL;
 			}
-			BytesToGo -= Chunk.Size;
+
+			switch( Chunk.ID )
+			{
+				case MESH_CHUNK:
+				{
+					if( MeshIndex > ( m_MeshCount - 1 ) )
+					{
+						std::cout << "[FPS::Model::LoadFromFile] <ERORR> "
+							"An unexpected mesh was encountered (only " <<
+							m_MeshCount << " meshes in the model)" <<
+							std::endl;
+
+						return FPS_FAIL;
+					}
+
+					if( ReadMeshData( pModelFile, MeshIndex, BytesRead ) !=
+						FPS_OK )
+					{
+						std::cout << "[FPS::Model::LoadFromFile] <ERROR> "
+							"Failed to read mesh data" << std::endl;
+
+						return FPS_FAIL;
+					}
+					break;
+				}
+				default:
+				{
+					std::cout << "[FPS::Model::LoadFromFile] <ERROR> Unknown "
+						"chunk: " << Chunk.ID << std::endl;
+
+					return FPS_FAIL;
+				}
+			}
+			BytesToGo -= BytesRead;
 		}
 
 		if( pModelFile )
@@ -80,12 +115,67 @@ namespace FPS
 		return FPS_FAIL;
 	}
 
-	FPS_UINT32 Model::ReadChunk( FILE_CHUNK &p_Chunk )
+	FPS_UINT32 Model::ReadChunk( FILE_CHUNK &p_Chunk, FILE *p_pFile )
 	{
-		std::cout << "[FPS::Model::ReadChunk] <ERROR> NOT IMPLEMENTED" <<
-			std::endl;
+		fread( &p_Chunk, sizeof( p_Chunk ), 1, p_pFile );
 
-		return FPS_FAIL;
+		return FPS_OK;
+	}
+
+	FPS_UINT32 Model::ReadMeshData( FILE *p_pFile,
+		const FPS_UINT32 p_MeshIndex, FPS_UINT32 &p_BytesRead )
+	{
+		FPS_UINT32 BytesRead = 0;
+
+		MESH_INFORMATION MeshInfo;
+		fread( &MeshInfo, sizeof( MeshInfo ), 1, p_pFile );
+		BytesRead += sizeof( MeshInfo );
+
+		FPS_BYTE *pVertices = new FPS_BYTE[ MeshInfo.VertexCount ];
+		fread( pVertices, sizeof( MODEL_VERTEX ), MeshInfo.VertexCount,
+			p_pFile );
+		BytesRead += sizeof( MODEL_VERTEX ) * MeshInfo.VertexCount;
+
+		FPS_UINT16 *pIndices = new FPS_UINT16[ MeshInfo.IndexCount ];
+		fread( pIndices, sizeof( FPS_UINT16 ), MeshInfo.IndexCount, p_pFile );
+		BytesRead += sizeof( FPS_UINT16 ) * MeshInfo.IndexCount;
+
+		m_pRenderer->RegisterPolygons( MeshInfo.VertexCount,
+			MeshInfo.IndexCount, pVertices, pIndices, GL_TRIANGLES,
+			0x66, m_pMeshData[ p_MeshIndex ].ID );
+
+		m_pMeshData[ p_MeshIndex ].MaterialMD5 = MeshInfo.MaterialMD5;
+
+		FILE_CHUNK Chunk;
+		if( ReadChunk( Chunk, p_pFile ) != FPS_OK )
+		{
+			std::cout << "[FPS::Model::ReadMeshData] <ERROR> Failed to read "
+				"end chunk" << std::endl;
+
+			return FPS_FAIL;
+		}
+
+		if( Chunk.ID != FILE_END_CHUNK )
+		{
+			std::cout << "[FPS::Model::ReadMeshData] <ERROR> End chunk ID is "
+				"not correct" << std::endl;
+
+			return FPS_FAIL;
+		}
+
+		if( Chunk.Size != 0 )
+		{
+			std::cout << "[FPS::Model::ReadMeshData] <ERROR> End chunk size "
+				"is not correct" << std::endl;
+
+			return FPS_FAIL;
+		}
+
+		BytesRead += sizeof( Chunk );
+
+		p_BytesRead = BytesRead;
+
+		return FPS_OK;
 	}
 }
 
